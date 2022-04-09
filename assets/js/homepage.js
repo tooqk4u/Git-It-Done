005a846d9b6d4b60741341ce406a2533aa95fae1
tooqk4u <tooqk4u@protonmail.com>
2022-04-09 16:55:06 -0600

    refined fetch call for users

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,11 @@
-let getUserRepos = function() {
-  fetch("https://api.github.com/users/octocat/repos");
-};
+let getUserRepos = function(user) {//format the github api url
+  let apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-getUserRepos()
+  //make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  })
+};
+getUserRepos("tooqk4u")
